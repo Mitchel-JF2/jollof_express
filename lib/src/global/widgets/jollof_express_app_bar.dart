c4c72c2ce8e35b650ae34dff4127f786d2873dee
Mitchel <febemitch1@gmail.com>
2023-11-17 17:00:54 +0100

    fixed authentication flow for google sign on ios

--- lib/src/global/widgets/jollof_express_app_bar.dart
+++ lib/src/global/widgets/jollof_express_app_bar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:jollof_express/src/config/themes/app_colors.dart';
 
+/// 
 class JollofExpressAppBar extends StatelessWidget
     implements PreferredSizeWidget {
   const JollofExpressAppBar(this.title, {super.key});
